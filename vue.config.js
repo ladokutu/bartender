b6482764140665eabed1614fcf197887e785384b
dj0nr1kk0 <djon.rikko@gmail.com>
2022-02-03 13:38:48 +0700

    add dist

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  
-  publicPath: '/bartender/',
-  transpileDependencies: [
-    'vuetify'
-  ]
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/bartender/'
+    : '/'
 }
